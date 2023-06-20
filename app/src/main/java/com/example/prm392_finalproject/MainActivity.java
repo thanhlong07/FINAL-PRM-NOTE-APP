1027905e64f2425573f6e8d0583b76fb719b3cef
thanhienee <hienntthe160135@fpt.edu.vn>
2023-06-20 22:52:57 +0700

    add new note layout "note" object

--- app/src/main/java/com/example/prm392_finalproject/MainActivity.java
+++ app/src/main/java/com/example/prm392_finalproject/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.prm392_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton addNoteBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addNoteBtn = findViewById(R.id.add_note_btn);
+
+        addNoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NoteDetailsActivity.class); //sang man hinh thu hai bang nut bam
+                startActivity(intent);
+            }
+        });
     }
 }
